2fc21df911ea36d1179dd8cc9ebc17e22c82e7b1
bcross97 <pipeborrerocruz@gmail.com>
2018-12-04 20:19:33 -0500

    Cambio de nombre del proyecto

--- src/co/edu/icesi/mio/bean/ConductorBean.java
+++ src/co/edu/icesi/mio/bean/ConductorBean.java
@@ -1,14 +1,9 @@
 package co.edu.icesi.mio.bean;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpSession;
 
 @Named("conductor")
 @SessionScoped
@@ -21,6 +16,4 @@ public class ConductorBean implements Serializable {
 	private String cedula;
 	private String apellidos;
 
-
-	
 }
